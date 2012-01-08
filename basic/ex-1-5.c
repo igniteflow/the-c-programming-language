08718ab283459e8137a6173a4617d825626a7705
Phil Tysoe <philtysoe@gmail.com>
2012-01-08 01:25:18 +0000

    Replaced the magic numbers with symbolic constants

--- basic/ex-1-5.c
+++ basic/ex-1-5.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
 
+#define LOWER	0
+#define UPPER 	300
+#define STEP 	10
+
 main() 
 {
   int fahr;
 
-  for (fahr = 300; fahr >= 0; fahr = fahr - 20)
+  for (fahr = UPPER; fahr >= LOWER; fahr = fahr - STEP)
     printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
